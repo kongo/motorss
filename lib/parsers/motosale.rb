3e84b1fa865fdf8d366a3e490a2afad97602a522
kongo <d.parshenko@gmail.com>
2013-10-10 08:29:08 +0300

    Motosale parser added

--- lib/parsers/motosale.rb
+++ lib/parsers/motosale.rb
@@ -1,21 +1,39 @@
+require 'string_extensions.rb'
 module Parsers
   class Motosale
     require 'net/http'
 
-    ENDPOINT = "www.motosale.com.ua/"
+    ENDPOINT = "www.motosale.com.ua"
     PER_PAGE = 10
 
     def fetch_list(page_num)
       doc = Nokogiri::HTML fetch_list_page_body(page_num)
+      items = doc.xpath('//body/table[1]/tr[2]/td[1]/table[1]/tr[1]/td[3]/table[1]/tr[1]/td[1]/div[3]/div[not(@id)]')
+
+      items.map do |item|
+        brand_model = item.xpath("div[2]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tr[3]/td/span/strong").first.content.strip
+
+        {
+          brand_name:     brand_model.split(" - ").first,
+          model_name:     brand_model.split(" - ").last,
+          mileage:        item.xpath("div[2]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tr[5]/td").first.children[1].content.strip.gsub(' км,', ''),
+          year_built:     item.xpath("div[2]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tr[5]/td").first.children[3].content.strip,
+          papers:         item.xpath("div[2]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tr[4]/td").first.children[1].content.strip,
+          link:           item.xpath("div[2]/table[1]/tbody[1]/tr[1]/td[1]/table/tr/td/a").first.attributes["href"].value,
+          price:          item.xpath("div[1]/table/tbody/tr/td[2]/b").first.content.strip,
+          date_published: begin item.xpath("div[2]/table[2]/tbody/tr/td").first.children[1].content.strip rescue next end
+        }
+      end.compact
     end
 
-    private
+    # private
 
-    def fetch_list_page_body(page_num)
-      url = ENDPOINT + "index.php?search=moto&model=&price%5Bmin%5D=&price"
-        + "%5Bmax%5D=&city=&in%5Bmin%5D=&in%5Bmax%5D=&run=&v=&type_obj=1&offset="
-        + (page_num ? page_num.to_s : "show_all")
-      Net::HTTP.get url
+    def fetch_list_page_body(page_num = 1)
+      offset = (page_num ? ((page_num - 1) * PER_PAGE).to_s : "show_all")
+      page_addr = "/index.php?search=moto&model=&price%5Bmin%5D=&price" +
+        "%5Bmax%5D=&city=&in%5Bmin%5D=&in%5Bmax%5D=&run=&v=&type_obj=1&offset=" +
+        (page_num ? page_num.to_s : "show_all")
+      Net::HTTP.get ENDPOINT, page_addr
     end
 
   end
--- spec/parsers/motosale_spec.rb
+++ spec/parsers/motosale_spec.rb
@@ -1,5 +1,117 @@
 require 'spec_helper'
 
 describe Parsers::Motosale do
+  describe "#fetch_list" do
+    it "fetches the first page of the list of motos on sale" do
+      Net::HTTP.stub(:get).and_return File.read("./spec/assets/list.html").force_encoding("CP1251")
 
+      m = Parsers::Motosale.new
+      list = m.fetch_list nil
+
+      list.should == [
+        {
+          :brand_name     => "SUZUKI",
+          :model_name     => "Boulevard",
+          :mileage       => "20500",
+          :year_built     => "2002",
+          :papers         => "Справка-счет на покупателя",
+          :link           => "suzuki/Boulevard_289940.html",
+          :price          => "6200 у.е.",
+          :date_published => "10.10.2013 "
+        },
+        {
+          :brand_name     => "HONDA",
+          :model_name     => "CRF 450 R",
+          :mileage       => "км,",
+          :year_built     => "2006",
+          :papers         => "Без документов",
+          :link           => "honda/CRF_450_R_289938.html",
+          :price          => "Дог. у.е.",
+          :date_published => "10.10.2013 "
+        },
+        {
+          :brand_name     => "SUZUKI",
+          :model_name     => "GSX 650 F",
+          :mileage       => "км,",
+          :year_built     => "1999",
+          :papers         => "Стоит на укр.учете",
+          :link           => "suzuki/GSX_650_F_289936.html",
+          :price          => "Дог. у.е.",
+          :date_published => "10.10.2013 "
+        },
+        {
+          :brand_name     => "KAWASAKI",
+          :model_name     => "VN 900",
+          :mileage       => "25000",
+          :year_built     => "2008",
+          :papers         => "Стоит на укр.учете",
+          :link           => "kawasaki/VN_900_289935.html",
+          :price          => "8500 у.е.",
+          :date_published => "10.10.2013 "
+        },
+        {
+          :brand_name     => "VIPER",
+          :model_name     => "ATV",
+          :mileage       => "10000",
+          :year_built     => "2008",
+          :papers         => "Справка-счет на покупателя",
+          :link           => "viper/ATV_289934.html",
+          :price          => "Дог. у.е.",
+          :date_published => "10.10.2013 "
+        },
+        {
+          :brand_name     => "SUZUKI",
+          :model_name     => "RF 600 R",
+          :mileage       => "36000",
+          :year_built     => "1998",
+          :papers         => "Стоит на укр.учете",
+          :link           => "suzuki/RF_600_R_289933.html",
+          :price          => "3200 у.е.",
+          :date_published => "10.10.2013 "
+        },
+        {
+          :brand_name     => "ZONGSHEN",
+          :model_name     => "200",
+          :mileage       => "8000",
+          :year_built     => "2008",
+          :papers         => "Стоит на укр.учете",
+          :link           => "zongshen/200_289932.html",
+          :price          => "1300 у.е.",
+          :date_published => "10.10.2013 "
+        },
+        {
+          :brand_name     => "JAWA",
+          :model_name     => "638",
+          :mileage       => "5000",
+          :year_built     => "1991",
+          :papers         => "Снят с учета",
+          :link           => "jawa/638_289931.html",
+          :price          => "550 у.е.",
+          :date_published => "10.10.2013 "
+        },
+        {
+          :brand_name     => "HONDA",
+          :model_name     => "Varadero 125",
+          :mileage       => "3720",
+          :year_built     => "2001",
+          :papers         => "Стоит на укр.учете",
+          :link           => "honda/Varadero_125_289930.html",
+          :price          => "3150 у.е.",
+          :date_published => "10.10.2013 "
+        },
+        {
+          :brand_name     => "ИЖ",
+          :model_name     => "юпітєр 5",
+          :mileage       => "40",
+          :year_built     => "1987",
+          :papers         => "Стоит на укр.учете",
+          :link           => "izh/yupіtier_5_289929.html",
+          :price          => "1500 у.е.",
+          :date_published => "10.10.2013 "
+        }
+      ]
+
+    end
+  end
 end
+
