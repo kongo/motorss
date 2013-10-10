8a996c1ce1a5b2deb28e3bdbb45a9acab7fe12fc
kongo <d.parshenko@gmail.com>
2013-10-10 03:57:47 +0300

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Motorss::Application.config.secret_key_base = 'c03cd8511130008b160c08326045d1bdfbb4d7df2eeb4d6dab82a3e8051644f2e240c841d61407fc73ea1b58d3826f83ba1e29719ec2832d24a9e703e5783408'
