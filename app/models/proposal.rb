3c04c674062100c2f13eab6616b833b177abf419
kongo <d.parshenko@gmail.com>
2013-10-10 09:38:14 +0300

    Default scope orders by date published desc

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,5 +1,5 @@
 class Proposal < ActiveRecord::Base
-  default_scope ->() { order 'id desc' }
+  default_scope ->() { order 'date_published desc' }
 
   def title
     "#{make} - #{model_name} (#{year_built}) - #{price}"
