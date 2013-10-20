6fab0ae4c4d593811668d92c4996def97265b955
kongo <d.parshenko@gmail.com>
2013-10-21 02:19:44 +0300

    Started working on UI (simple_form, bootstrap)

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,9 +1,35 @@
 class Proposal < ActiveRecord::Base
 
-  VEHICLE_TYPES = [
-    :classic, :neoclassic, :chopper, :sport, :sporttourist, :tourist,
-    :enduro, :cross, :pitbike, :supermoto, :trial, :scooter,
-    :maxiscooter, :custom, :trike, :quadracycle, :watercraft, :snowmobile
+  VEHICLE_TYPES_NAMES = {
+    classic:        'Классик',
+    neoclassic:     'Неоклассик',
+    chopper:        'Чоппер',
+    sport:          'Спорт',
+    sporttourist:   'Спорт-турист',
+    tourist:        'Турист',
+    enduro:         'Эндуро',
+    cross:          'Кросс',
+    pitbike:        'Питбайк',
+    supermoto:      'Супермото',
+    trial:          'Триал',
+    scooter:        'Скутер',
+    maxiscooter:    'Максискутер',
+    custom:         'Кастом',
+    trike:          'Трайк',
+    quadracycle:    'Квадроцикл',
+    watercraft:     'Гидроцикл',
+    snowmobile:     'Снегоход'
+  }
+  VEHICLE_TYPES = VEHICLE_TYPES_NAMES.keys
+
+  PAPERS_TYPES_NAMES = [
+    "Модель не для дорог общего пользования",
+    "Не растаможен(есь зарубежные доки)",
+    "\"Свои\" документы(деланые)",
+    "Без документов",
+    "Снят с учета",
+    "Стоит на укр.учете",
+    "Справка-счет на покупателя"
   ]
 
   validates_inclusion_of   :vehicle_type, in: VEHICLE_TYPES
